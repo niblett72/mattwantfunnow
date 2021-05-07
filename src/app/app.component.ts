926db6bdb6332c5e73b730cdfb51a3754fd26a33
James Niblett <niblett72@gmail.com>
2021-05-07 07:22:10 -0500

    Added Matt as the actual subject.  Redesigned the fun meter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,8 @@ export interface IfunLevel {
     level: number,
     meters: number[],
     message: string,
-    icon: string
+    icon: string,
+    image: string
 }
 @Component({
   selector: 'app-root',
@@ -11,10 +12,10 @@ export interface IfunLevel {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    funMeter: number = 20;
-    funLevel: IfunLevel = this.getFunLevel(this.funMeter);
+    funTimes: number = 20;
+    funLevel: IfunLevel = this.getFunLevel(this.funTimes);
     addFunTimes(value: number) {
-        this.funMeter = value;
+        this.funTimes = value;
     }
     setFunLevel(val: number): void {
         this.funLevel = this.getFunLevel(val);
@@ -24,27 +25,32 @@ export class AppComponent {
             level: 4,
             meters: [16, 17, 18, 19, 20],
             message: "Matt is having fun!",
-            icon: "mood"
+            icon: "mood",
+            image: "/assets/images/4.matt-fun.png"
         }, {
             level: 3,
             meters: [11, 12, 13, 14, 15],
             message: "Matt is whelmed by the fun",
-            icon: "sentiment_satisfied"
+            icon: "sentiment_satisfied",
+            image: "/assets/images/3.matt-whelmed.png"
         }, {
             level: 2,
             meters: [6, 7, 8, 9, 10],
             message: "Matt needs fun times, now!",
-            icon: "sentiment_neutral"
+            icon: "sentiment_neutral",
+            image: "/assets/images/2.matt-needsfun.png"
         }, {
             level: 1,
             meters: [1, 2, 3, 4, 5],
             message: "Matt will power down soon...",
-            icon: "sentiment_very_dissatisfied"
+            icon: "sentiment_very_dissatisfied",
+            image: "/assets/images/1.matt-poweringdown.png"
         }, {
             level: 0,
             meters: [0],
-            message: "Matt has powered down...",
-            icon: "mood_bad"
+            message: "You let Matt power down!",
+            icon: "mood_bad",
+            image: "/assets/images/0.matt-powerdown.png"
         }];
         return funLevels.filter(fl => fl.meters.includes(meter))[0];
     }
